51841d05e6e86810ad0cadaea429eb72944a36a6
weichao88 <1074856319@qq.com>
2019-10-19 16:18:54 +0800

    .....
    .....

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,26 +3,13 @@ package com.example.myapplication;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
-
 import android.content.Intent;
-=======
-<<<<<<< HEAD
->>>>>>> 41320e113ce267359428627e3ac5326c1a281517
+
 import android.media.Image;
 import android.app.Activity;
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
+
 import android.content.Intent;
-=======
->>>>>>> 17e59519b70518cc36dfcd8e2648445ce94b7fbf
->>>>>>> d6ac82212445fedb7cfbdd4e42083771932ae317
->>>>>>> 36715cc90a7da4882601e5b1ae91abf25485a500
->>>>>>> 41320e113ce267359428627e3ac5326c1a281517
+
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -44,12 +31,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
-=======
->>>>>>> 41320e113ce267359428627e3ac5326c1a281517
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.FormBody;
@@ -58,7 +40,7 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
->>>>>>> 36715cc90a7da4882601e5b1ae91abf25485a500
+
 
 public class MainActivity extends AppCompatActivity {
     Handler handler = null;
@@ -67,9 +49,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
-
-=======
 //        Intent intent=getIntent();
 //        Bundle bundle=intent.getExtras();
 //        String string=bundle.getString("text");
@@ -83,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
->>>>>>> 36715cc90a7da4882601e5b1ae91abf25485a500
+
     }
 
 }
